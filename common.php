eadfdfaf4399911a68817c7c63901fe51b758e55
Sascha <Sascha@Arbeit>
2014-01-14 10:46:27 +0100

    Initial Commit

---
+++ common.php
@@ -0,0 +1,9 @@
+<?php
+include(__DIR__.'/paths.php');
+require_once PROJECT_DOCUMENT_ROOT.'/settings.php';
+require_once PROJECT_DOCUMENT_ROOT.'/inc/class/includeAllClasses.php';
+/*
+if(!isset($GLOBALS['DATABASE'])){
+    $DB = new SYSTEM\Database\MySQL(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+}
+*/
